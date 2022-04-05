ff63d9373090321b7734b100ee773ee140d8bfcf
carl beattie <12beattiecastp@gmail.com>
2022-04-05 14:14:28 +0100

    reworked coordinates.js

--- src/js/coordinates.js
+++ src/js/coordinates.js
@@ -1,50 +1,27 @@
-import config from "./canvas.config";
+import shared_state from "./shared_state";
 
-class CursorCoordinates {
+class Coordinates {
 
-  constructor({view_container, canvas, coordinatesGUI}) {
+  constructor(canvas, coordinates_display) {
 
-    this.view_container = document.getElementById(view_container);
-    this.canvas = document.getElementById(canvas);
-    this.coordinatesGUI = document.getElementById(coordinatesGUI);
-    this.x = 0;
-    this.y = 0;
+    this.canvas = canvas;
+    this.coordinates_display = coordinates_display;
 
   }
 
   init() {
 
-    this.coordinatesGUI.style.display = "unset"
-
-    const coordinatesTextElement = document.createElement("p");
-
-    coordinatesTextElement.textContent = `(${this.x},${this.y})`;
-
-    this.coordinatesGUI.append(coordinatesTextElement);
-
-    this.canvasHovering();
+    this.getCoordinatesOnMouseOverCanvas();
 
   }
 
-  canvasHovering() {
 
-    this.view_container.addEventListener("mousemove", (e) => {
-
-      const { x, y } = this.getCoordinates(e);
-
-      this.coordinatesGUI.querySelector("p").textContent = `(${Math.floor(x)}, ${Math.floor(y)})`;
-
-    })
-
-  }
-
-  getCoordinates(e) {
+  getCursorCoordinates(e) {
 
     const rect = this.canvas.getBoundingClientRect();
 
-    // Take scaling into account
-    const scaleX = this.canvas.width / rect.width;
-    const scaleY = this.canvas.height / rect.height;
+    const scaleX = canvas.width / rect.width;
+    const scaleY = canvas.height / rect.height;
 
     return {
       x: (e.clientX - rect.left) * scaleX,
@@ -53,10 +30,31 @@ class CursorCoordinates {
 
   }
 
+  updateCoordinatesDisplay(x, y) {
+
+    x = Math.floor(x);
+    y = Math.floor(y);
+
+    this.coordinates_display.querySelector("p").textContent = `(${x}, ${y})`;
+
+  }
+
+  getCoordinatesOnMouseOverCanvas() {
+
+    this.canvas.addEventListener("mousemove", (e) => {
+
+      const { x, y } = this.getCursorCoordinates(e);
+
+      this.updateCoordinatesDisplay(x, y);
+
+    })
+
+  }
+
 }
 
-export default new CursorCoordinates({
-  view_container: config.HTML_ELEMENTS_IDS.place_camera,
-  canvas: config.HTML_ELEMENTS_IDS.place_canvas,
-  coordinatesGUI: config.HTML_ELEMENTS_IDS.cursor_position_container
-})
+const main = new Coordinates(shared_state.canvas, shared_state.coordinates_display);
+
+main.init();
+
+export default main;
