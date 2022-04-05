052d57a3dd8e1308ccd8278d3d925d8cbc741e99
carl beattie <12beattiecastp@gmail.com>
2022-04-05 14:13:00 +0100

    reworked canvas.js

--- src/js/canvas.js
+++ src/js/canvas.js
@@ -1,26 +1,25 @@
-import config from "./canvas.config";
+import shared_state from "./shared_state";
+import config from "./config";
 
 class Canvas {
+  constructor(canvas, width, height) {
 
-  constructor({width, height, canvas_id}) {
-    
-    this.width = width,
-    this.height = height,
-    this.canvas = document.getElementById(canvas_id);
+    this.canvas = canvas;
+    this.width = width;
+    this.height = height
 
   }
-  
+
   init() {
 
-    this.canvas.width = this.width;
     this.canvas.height = this.height;
+    this.canvas.width = this.width;
+    this.canvas.style.backgroundColor = "#fff";
+
+    shared_state.setCtx(this.canvas.getContext("2d"));
+    shared_state.setCanvas(this.canvas);
 
   }
-  
 }
 
-export default new Canvas({
-  width: config.CANVAS.WIDTH,
-  height: config.CANVAS.HEIGHT,
-  canvas_id: config.HTML_ELEMENTS_IDS.place_canvas
-})
+export default new Canvas(shared_state.canvas, config.canvas_width, config.canvas_height);
