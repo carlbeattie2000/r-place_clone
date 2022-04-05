1b969b07bb0adf67bf68a8c96f65e40801d05eb6
carl beattie <12beattiecastp@gmail.com>
2022-04-05 14:15:58 +0100

    updated index.js to support new reworks

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,13 @@
+import styles from "./styles.js";
 import canvas from "./canvas";
 import zoom from "./zoom";
 import coordinates from "./coordinates";
 import camera from "./camera";
-
-import "./styles.imports";
+import color_selector from "./color_selector";
+import draw from "./draw";
 
 canvas.init();
 zoom.init();
 coordinates.init();
 camera.init();
+color_selector.init();
